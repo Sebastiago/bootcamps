d1f7ecbd51dcbab86a102736343a525a892d3be6
Your Name <you@example.com>
2022-11-08 09:54:06 -0500

    validaciones

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,70 +1,146 @@
 const sequelize = require('../config/seq')
-const { DataTypes } = require('sequelize')
+const { DataTypes, ValidationError } = require('sequelize')
 const UserModel = require('../models/user')
 
 const User = UserModel(sequelize, DataTypes)
 
 
 //get: obtener datos R
-exports.traerUsers = async(req, res)=>{
-    const users = await User.findAll()
-    res.status(200).json(
-        {
-            "success" : true,
-            "data" : users
-        }
-    )
+exports.traerUsers = async (req, res) => {
+    try {
+        const users = await User.findAll()
+        res.status(200).json(
+            {
+                "success": true,
+                "data": users
+            }
+        )
+    } catch (error) {
+        res.status(422).json(
+            {
+                "success": false,
+                "errors": "error de servidor"
+            }
+        )
+    }
+
+
 }
 
-exports.traerUserPorId = async (req, res)=>{
-    const userId = await User.findByPk(req.params.id)
-    res.status(200).json(
-        {
-            "success" : true,
-            "data" : userId
+exports.traerUserPorId = async (req, res) => {
+    try {
+        const userId = await User.findByPk(req.params.id)
+        //si usuario no existe
+        if (!userId) {
+            res.status(422).json(
+                {
+                    "success": true,
+                    "errors": ['user no existe']
+                }
+            )
         }
-    )
+        res.status(200).json(
+            {
+                "success": true,
+                "data": userId
+            }
+        )
+    } catch (error) {
+        res.status(500).json(
+            {
+                "success": false,
+                "errors": "error de servidor"
+            }
+        )
+    }
+
 }
 
 //post: crear un nuevo recurso
-exports.crearUser =async (req, res)=>{
-    const newUser = await User.create(req.body);
-    res.status(201).json(
-        {
-            "success" : true,
-            "data" : newUser
+exports.crearUser = async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.status(200).json(
+            {
+                "success": true,
+                "data": newUser
+            }
+        )
+    } catch (error) {
+
+        if (error instanceof ValidationError) {
+            //llevar errores a response
+            //mensajes de error unicamente
+            const errores = error.errors.map((e) => e.message)
+            res.status(422).json(
+                {
+                    "success": false,
+                    "errors": errores
+                }
+            )
+        } else {
+            //errores de servidor
+            res.status(422).json(
+                {
+                    "success": false,
+                    "errors": "error de servidor"
+                }
+            )
         }
-    )
+    }
 }
 
 //put-patch: actualizar un recurso
-exports.ActualizarUser = async (req, res)=>{
-    await User.update(req.body, {
-        where:{
-            id: req.params.id
+exports.ActualizarUser = async (req, res) => {
+    try {
+        const upUser = await User.findByPk(req.params.id)
+        if (!upUser) {
+            res.status(422).json(
+                {
+                    "success": true,
+                    "errors": ['user no existe']
+                }
+            )
         }
-    })
-    const upUser = await User.findByPk(req.params.id)
-    res.status(200).json(
-        {
-            "succes" : true,
-            "data" : upUser
+        else {
+            await User.update(req.body, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            //seleccionar usuario actualizado
+            const userAct = await User.findByPk(req.params.id)
+            //enviar response cin usuario actualizado
+            res.status(200).json(
+                {
+                    "success": true,
+                    "data": userAct
+                }
+            )
         }
-    )
+    }
+    catch (error) {
+        res.status(422).json(
+            {
+                "success": false,
+                "errors": "error de servidor"
+            }
+        )
+    }
 }
 
 //delete: borrar un recurso
-exports.borrarUser =async (req, res)=>{
+exports.borrarUser = async (req, res) => {
     const borrar = await User.findByPk(req.params.id)
     await User.destroy({
         where: {
             id: req.params.id
         }
-      });
+    });
     res.status(200).json(
         {
-            "succes" : true,
-            "data" : borrar
+            "succes": true,
+            "data": borrar
         }
     )
 }
--- models/user.js
+++ models/user.js
@@ -14,9 +14,63 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isAlpha: {
+          args: true,
+          msg: 'Name solo debe tener letras'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Name no debe quedar vacio'
+        },
+        notNull: {
+          args: true,
+          msg: 'Name no debe quedar vacio'
+        },
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Email no cumple los requisitos'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Email no debe quedar vacio'
+        },
+        notNull: {
+          args: true,
+          msg: 'Name no debe quedar vacio'
+        },
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: {
+          args: [2,10],
+          msj: "Password solo permite de 2 a 10 caracteres"
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Password no debe quedar vacio'
+        },
+        notNull: {
+          args: true,
+          msg: 'Password no debe quedar vacio'
+        },
+      }
+    }
   }, {
     sequelize,
     modelName: 'User',
